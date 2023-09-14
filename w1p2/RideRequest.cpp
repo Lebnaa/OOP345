ab45066b6118668b9ee0a45a637b88626de221f2
Lebnaa <127699913+Lebnaa@users.noreply.github.com>
2023-09-14 16:10:58 -0400

    update

--- w1p2/RideRequest.cpp
+++ w1p2/RideRequest.cpp
@@ -3,6 +3,7 @@
 	Sections: NAA
 	Student ID: 157672205
 	Seneca Email: lnoori1@myseneca.ca
+	Date: Sept. 14/2023
 
 I have done all the coding by myself and only copied the code that 
 my professor provided to complete my workshops and assignments.
--- w1p2/RideRequest.h
+++ w1p2/RideRequest.h
@@ -1,3 +1,14 @@
+/*									Workshop 2 Part 1
+	Lebna Noori
+	Sections: NAA
+	Student ID: 157672205
+	Seneca Email: lnoori1@myseneca.ca
+	Date: Sept. 14/2023
+
+I have done all the coding by myself and only copied the code that
+my professor provided to complete my workshops and assignments.
+*/
+
 #ifndef SDDS_RIDEREQUEST_H
 #define SDDS_RIDEREQUEST_H
 #include <iostream>
