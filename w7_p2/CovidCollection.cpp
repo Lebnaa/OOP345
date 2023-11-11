460d07f64f140a803ccbc9e7f7b29b95ca7cc08d
Lebnaa <127699913+Lebnaa@users.noreply.github.com>
2023-11-11 02:55:37 -0500

    done

--- w7_p2/CovidCollection.cpp
+++ w7_p2/CovidCollection.cpp
@@ -18,61 +18,64 @@
 #include "CovidCollection.h"
 
 using namespace std;
+
 namespace sdds
 {
-	sdds::CovidCollection::CovidCollection(const char* filename)
+	sdds::CovidCollection::CovidCollection(const char* fileName)
 	{
-		fstream file(filename);
+		fstream file(fileName);
 
 		if (file)
 		{
 			//extract covid details from file
 			while (file.peek() != EOF)
 			{
-				Covid newCovid{};
-				string t_year{};
-				string t_cases{};
-				string t_deaths{};
+				Covid newCov{};
+				string tempYear{};
+				string tempCases{};
+				string tempDeaths{};
 				char temp[26]{};
-				char t_num[6]{};
+				char tempNum[6]{};
 
 				//read country
 				file.read(temp, 25);
-				newCovid.m_country = temp;
-				trimString(newCovid.m_country);
+				newCov.m_country = temp;
+				trimString(newCov.m_country);
 
 				//read city
 				file.read(temp, 25);
-				newCovid.m_city = temp;
-				trimString(newCovid.m_city);
+				newCov.m_city = temp;
+				trimString(newCov.m_city);
 
 				//read variant
 				file.read(temp, 25);
-				newCovid.m_variant = temp;
-				trimString(newCovid.m_variant);
+				newCov.m_variant = temp;
+				trimString(newCov.m_variant);
 
 				//read year
-				file.read(t_num, 5);
-				t_year = t_num;
-				trimString(t_year);
-				newCovid.m_year = stoi(t_year);
+				file.read(tempNum, 5);
+				tempYear = tempNum;
+				trimString(tempYear);
+				newCov.m_year = stoi(tempYear);
 
 				//read cases
-				file.read(t_num, 5);
-				t_cases = t_num;
-				trimString(t_cases);
-				newCovid.m_cases = stoul(t_cases);
+				file.read(tempNum, 5);
+				tempCases = tempNum;
+				trimString(tempCases);
+				newCov.m_cases = stoul(tempCases);
 
 				//read deaths
-				file >> t_deaths;
-				trimString(t_deaths);
-				newCovid.m_deaths = stoul(t_deaths);
+				file >> tempDeaths;
+				trimString(tempDeaths);
+				newCov.m_deaths = stoul(tempDeaths);
 
 				//ignore everything else untill newline
 				file.ignore(numeric_limits<std::streamsize>::max(), '\n');
 
+				//file >> newCov.m_country >> newCov.m_city >> newCov.m_variant >> tempYear >> tempCases >> tempDeaths;
+
 				//add the new covid to collection
-				CovidRecord.push_back(newCovid);
+				m_coll.push_back(newCov);
 			}
 		}
 		else //file could not be reached raise exception
@@ -81,85 +84,72 @@ namespace sdds
 		}
 	}
 
-	//part 2 
 	void CovidCollection::display(std::ostream& out, const std::string& country) const
 	{
-		const size_t totalCases = accumulate(CovidRecord.begin(), CovidRecord.end(), 0, [](size_t sum, const Covid& covid) 
-			{
-				return sum + covid.m_cases;
+		const size_t totalCases = accumulate(m_coll.begin(), m_coll.end(), 0, [](size_t sum, const Covid& cov) {
+			return sum + cov.m_cases;
 			});
-		const size_t totalDeaths = accumulate(CovidRecord.begin(), CovidRecord.end(), 0, [](size_t sum, const Covid& covid) 
-			{
-				return sum + covid.m_deaths;
+		const size_t totalDeaths = accumulate(m_coll.begin(), m_coll.end(), 0, [](size_t sum, const Covid& cov) {
+			return sum + cov.m_deaths;
 			});
 
 		if (country == "ALL")
 		{
-			for_each(CovidRecord.begin(), CovidRecord.end(), [=, &out](Covid covid) {out << covid << endl; });
+			for_each(m_coll.begin(), m_coll.end(), [=, &out](Covid cov) {out << cov << endl; }); 
+			//print all covids
 			out << "|" << setw(80) << setiosflags(ios::right) << "Total cases around the world: " << totalCases << " |\n"
 				<< "|" << setw(80) << "Total deaths around the world: " << totalDeaths << " |\n" << resetiosflags(ios::right);
 		}
 		else
 		{
-			const size_t localCases = accumulate(CovidRecord.begin(), CovidRecord.end(), 0, [country](size_t sum, const Covid& covid) 
-				{
-					if (covid.m_country == country)
-					{
-						return sum + covid.m_cases;
-					}
-					else
-					{
-						return sum;
-					}
+			const size_t localCases = accumulate(m_coll.begin(), m_coll.end(), 0, [country](size_t sum, const Covid& cov) {
+				if (cov.m_country == country)
+					return sum + cov.m_cases;
+				else
+					return sum;
 				});
-			const size_t localDeaths = accumulate(CovidRecord.begin(), CovidRecord.end(), 0, [country](size_t sum, const Covid& covid)
-				{
-					if (covid.m_country == country)
-					{
-						return sum + covid.m_deaths;
-					}
-					else
-					{
-						return sum;
-					}
+			const size_t localDeaths = accumulate(m_coll.begin(), m_coll.end(), 0, [country](size_t sum, const Covid& cov) {
+				if (cov.m_country == country)
+					return sum + cov.m_deaths;
+				else
+					return sum;
 				});
 
-			stringstream TotalCases{};
-			stringstream TotalDeaths{};
-			stringstream TotalPercent{};
+			stringstream TotCases{};
+			stringstream TotDeaths{};
+			stringstream TotPercent{};
 			out << "Displaying information of country = " << country << "\n";
-			for_each(CovidRecord.begin(), CovidRecord.end(), [=, &out, &localCases, &localDeaths](Covid covid)
+			for_each(m_coll.begin(), m_coll.end(), [=, &out, &localCases, &localDeaths](Covid cov) 
+				//print all covids for given country
 				{
-					if (covid.m_country == country)
-						out << covid << endl;;
+					if (cov.m_country == country)
+						out << cov << endl;;
 				});
 			out << setfill('-') << setw(89) << '\n' << setfill(' ');
-			TotalCases << "Total cases in " << country << ": " << localCases;
-			out << "|" << setw(85) << setiosflags(ios::right) << TotalCases.str() << " |\n";
-			TotalDeaths << "Total deaths in " << country << ": " << localDeaths;
-			out << "|" << setw(85) << TotalDeaths.str() << " |\n";
-			TotalPercent << country << " has " << setprecision(6) << setiosflags(ios::fixed) << ((double)localCases / totalCases) * 100 << "% of global cases and " << ((double)localDeaths / totalDeaths) * 100 << "% of global deaths";
-			out << "|" << setw(85) << TotalPercent.str() << " |\n"
+			TotCases << "Total cases in " << country << ": " << localCases;
+			out << "|" << setw(85) << setiosflags(ios::right) << TotCases.str() << " |\n";
+			TotDeaths << "Total deaths in " << country << ": " << localDeaths;
+			out << "|" << setw(85) << TotDeaths.str() << " |\n";
+			TotPercent << country << " has " << setprecision(6) << setiosflags(ios::fixed) << ((double)localCases / totalCases) * 100 << "% of global cases and " << ((double)localDeaths / totalDeaths) * 100 << "% of global deaths";
+			out << "|" << setw(85) << TotPercent.str() << " |\n"
 				<< resetiosflags(ios::right);
 		}
 	}
 
 	void CovidCollection::sort(const std::string& field)
 	{
-		//for city 
 		if (field == "city")
 		{
-			std::sort(CovidRecord.begin(), CovidRecord.end(), [=](const Covid& a, const Covid& b) 
+			std::sort(m_coll.begin(), m_coll.end(), [=](const Covid& a, const Covid& b) {
+				if (a.m_city.compare(b.m_city) < 0)
 				{
-					if (a.m_city.compare(b.m_city) < 0)
-					{
-						return true;
-					}
-					else if (a.m_city.compare(b.m_city) > 0)
-					{
-						return false;
-					}
-				else 
+					return true;
+				}
+				else if (a.m_city.compare(b.m_city) > 0)
+				{
+					return false;
+				}
+				else //they are the same
 				{
 					if (a.m_deaths < b.m_deaths)
 					{
@@ -174,91 +164,83 @@ namespace sdds
 		}
 		else if (field == "variant")
 		{
-			//for variant 
-			std::sort(CovidRecord.begin(), CovidRecord.end(), [=](const Covid& a, const Covid& b) 
+			std::sort(m_coll.begin(), m_coll.end(), [=](const Covid& a, const Covid& b) {
+				if (a.m_variant.compare(b.m_variant) < 0)
 				{
-					if (a.m_variant.compare(b.m_variant) < 0)
+					return true;
+				}
+				else if (a.m_variant.compare(b.m_variant) > 0)
+				{
+					return false;
+				}
+				else //they are the same
+				{
+					if (a.m_deaths < b.m_deaths)
 					{
 						return true;
 					}
-					else if (a.m_variant.compare(b.m_variant) > 0)
+					else if (a.m_deaths > b.m_deaths)
 					{
 						return false;
 					}
-					else 
+					else //same variant and same year 
 					{
-						if (a.m_deaths < b.m_deaths)
-						{
-							return true;
-						}
-						else if (a.m_deaths > b.m_deaths)
-						{
-							return false;
-						}
-						else 
-						{
-							return a.m_year < b.m_year;
-						}
+						return a.m_year < b.m_year;
 					}
+				}
 				});
 		}
 		else if (field == "year")
 		{
-			//for year 
-			std::sort(CovidRecord.begin(), CovidRecord.end(), [=](const Covid& a, const Covid& b) 
+			std::sort(m_coll.begin(), m_coll.end(), [=](const Covid& a, const Covid& b) {
+				if (a.m_year < b.m_year)
 				{
-					if (a.m_year < b.m_year)
+					return true;
+				}
+				else if (a.m_year > b.m_year)
+				{
+					return false;
+				}
+				else
+				{
+					if (a.m_deaths < b.m_deaths)
 					{
 						return true;
 					}
-					else if (a.m_year > b.m_year)
+					else
 					{
 						return false;
 					}
-					else
-					{
-						if (a.m_deaths < b.m_deaths)
-						{
-							return true;
-						}
-						else
-						{
-							return false;
-						}	
-					}
+				}
 				});
 		}
 		else if (field == "cases")
 		{
-			//for cases 
-			std::sort(CovidRecord.begin(), CovidRecord.end(), [=](const Covid& a, const Covid& b) 
+			std::sort(m_coll.begin(), m_coll.end(), [=](const Covid& a, const Covid& b) {
+				if (a.m_cases > b.m_cases)
 				{
-					if (a.m_cases > b.m_cases)
+					return true;
+				}
+				else if (a.m_cases < b.m_cases)
+				{
+					return false;
+				}
+				else
+				{
+					if (a.m_deaths < b.m_deaths)
 					{
 						return true;
 					}
-					else if (a.m_cases < b.m_cases)
+					else
 					{
 						return false;
 					}
-					else
-					{
-						if (a.m_deaths < b.m_deaths)
-						{
-							return true;
-						}
-						else
-						{
-							return false;
-						}
-					}
+				}
 				});
 		}
 		else if (field == "deaths")
 		{
-			//for deaths 
-			std::sort(CovidRecord.begin(), CovidRecord.end(), [=](const Covid& a, const Covid& b) 
-			{
+			std::sort(m_coll.begin(), m_coll.end(), [=](const Covid& a, const Covid& b) {
 				if (a.m_deaths > b.m_deaths)
 				{
 					return true;
@@ -267,13 +249,11 @@ namespace sdds
 				{
 					return false;
 				}
-			});
+				});
 		}
 		else if (field == "severity")
 		{
-			//for severity
-			std::sort(CovidRecord.begin(), CovidRecord.end(), [=](const Covid& a, const Covid& b) 
-			{
+			std::sort(m_coll.begin(), m_coll.end(), [=](const Covid& a, const Covid& b) {
 				if (a.m_severity.compare(b.m_severity) < 0)
 				{
 					return true;
@@ -282,7 +262,7 @@ namespace sdds
 				{
 					return false;
 				}
-				else 
+				else //they are the same
 				{
 					if (a.m_deaths < b.m_deaths)
 					{
@@ -293,13 +273,11 @@ namespace sdds
 						return false;
 					}
 				}
-			});
+				});
 		}
-		else 
+		else //country
 		{
-			//for country
-			std::sort(CovidRecord.begin(), CovidRecord.end(), [=](const Covid& a, const Covid& b) 
-			{
+			std::sort(m_coll.begin(), m_coll.end(), [=](const Covid& a, const Covid& b) {
 				if (a.m_country.compare(b.m_country) < 0)
 				{
 					return true;
@@ -308,7 +286,7 @@ namespace sdds
 				{
 					return false;
 				}
-				else 
+				else //they are the same
 				{
 					if (a.m_deaths < b.m_deaths)
 					{
@@ -319,50 +297,41 @@ namespace sdds
 						return false;
 					}
 				}
-			});
+				});
 		}
 	}
 
 	bool CovidCollection::inCollection(const std::string& variant, const std::string& country, unsigned int deaths) const
 	{
 		return
-			any_of(CovidRecord.begin(), CovidRecord.end(), [=](const Covid& covid)
-				{
-					return covid.m_variant == variant && covid.m_country == country && covid.m_deaths > deaths;
+			any_of(m_coll.begin(), m_coll.end(), [=](const Covid& cov) {
+			return cov.m_variant == variant && cov.m_country == country && cov.m_deaths > deaths;
 				});
 	}
 
 	std::list<Covid> CovidCollection::getListForDeaths(unsigned int deaths) const
 	{
 		list<Covid> list{};
-		copy_if(CovidRecord.begin(), CovidRecord.end(), std::back_inserter(list), [=](const Covid& covid) -> bool
+		copy_if(m_coll.begin(), m_coll.end(), std::back_inserter(list), [=](const Covid& cov) -> bool
 			{
-				return covid.m_deaths >= deaths;
+				return cov.m_deaths >= deaths;
 			});
 		return list;
 	}
 
 	void CovidCollection::updateStatus()
 	{
-		transform(CovidRecord.begin(), CovidRecord.end(), CovidRecord.begin(), [](const Covid& covid)
-		{
-				Covid newCov = covid;
-				
-				if (covid.m_deaths > EPIDEMIC_DEATHS)
-				{
-					newCov.m_severity = "EPIDEMIC";
-				}
-				else if (covid.m_deaths < EARLY_DEATHS)
-				{
-					newCov.m_severity = "EARLY";
-				}
-				else
-				{
-					newCov.m_severity = "MILD";
-
-				}
+		transform(m_coll.begin(), m_coll.end(), m_coll.begin(), [](const Covid& cov) {
+			Covid newCov = cov;
+			if (cov.m_deaths > EPIDEMIC_DEATHS)
+				newCov.m_severity = "EPIDEMIC";
+			else if (cov.m_deaths < EARLY_DEATHS)
+				newCov.m_severity = "EARLY";
+			else
+				newCov.m_severity = "MILD";
 			return newCov;
-		});
+			});
+		//std::copy(copy.begin(), copy.end(), m_coll.begin());
 	}
 
 	void CovidCollection::trimString(std::string& source)
@@ -380,13 +349,13 @@ namespace sdds
 		}
 		source = res;
 	}
-
 	std::ostream& operator<<(std::ostream& out, const Covid& theCovid)
 	{
 		out << "| " << setw(21) << std::left << theCovid.m_country << " | "
 			<< setw(15) << theCovid.m_city << " | "
 			<< setw(20) << theCovid.m_variant << " | " << resetiosflags(ios::left)
 			<< setw(6);
+		//print the year if positive
 		if (theCovid.m_year > 0)
 		{
 			out << theCovid.m_year << " | ";
@@ -402,7 +371,6 @@ namespace sdds
 
 		return out;
 	}
-
 	bool operator<(const std::string& a, const std::string& b)
 	{
 		return false;
